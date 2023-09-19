1b9b56a67a3e18db57a56c74459cf2d12e649a34
Aliaksei Karatkevich <abk@krononsoft.com>
2023-09-19 11:32:26 +0300

    chars + weight

--- src/test/scala/patmat/HuffmanSuite.scala
+++ src/test/scala/patmat/HuffmanSuite.scala
@@ -1,23 +1,25 @@
 package patmat
 
 class HuffmanSuite extends munit.FunSuite:
+
   import Huffman.*
 
   trait TestTrees {
-    val t1 = Fork(Leaf('a',2), Leaf('b',3), List('a','b'), 5)
-    val t2 = Fork(Fork(Leaf('a',2), Leaf('b',3), List('a','b'), 5), Leaf('d',4), List('a','b','d'), 9)
+    val t1 = Fork(Leaf('a', 2), Leaf('b', 3), List('a', 'b'), 5)
+    val t2 = Fork(Fork(Leaf('a', 2), Leaf('b', 3), List('a', 'b'), 5), Leaf('d', 4), List('a', 'b', 'd'), 9)
   }
 
 
   test("weight of a larger tree (10pts)") {
     new TestTrees:
+      //      println(t1)
       assertEquals(weight(t1), 5)
   }
 
 
   test("chars of a larger tree (10pts)") {
     new TestTrees:
-      assertEquals(chars(t2), List('a','b','d'))
+      assertEquals(chars(t2), List('a', 'b', 'd'))
   }
 
   test("string2chars hello world") {
@@ -26,13 +28,13 @@ class HuffmanSuite extends munit.FunSuite:
 
 
   test("make ordered leaf list for some frequency table (15pts)") {
-    assertEquals(makeOrderedLeafList(List(('t', 2), ('e', 1), ('x', 3))), List(Leaf('e',1), Leaf('t',2), Leaf('x',3)))
+    assertEquals(makeOrderedLeafList(List(('t', 2), ('e', 1), ('x', 3))), List(Leaf('e', 1), Leaf('t', 2), Leaf('x', 3)))
   }
 
 
   test("combine of some leaf list (15pts)") {
     val leaflist = List(Leaf('e', 1), Leaf('t', 2), Leaf('x', 4))
-    assertEquals(combine(leaflist), List(Fork(Leaf('e',1),Leaf('t',2),List('e', 't'),3), Leaf('x',4)))
+    assertEquals(combine(leaflist), List(Fork(Leaf('e', 1), Leaf('t', 2), List('e', 't'), 3), Leaf('x', 4)))
   }
 
 
@@ -43,4 +45,5 @@ class HuffmanSuite extends munit.FunSuite:
 
 
   import scala.concurrent.duration.*
+
   override val munitTimeout = 10.seconds
